d1423aed182891598360f37bbf4aa1a623b6cc7d
gretmn102 <erlgreywatson@gmail.com>
2023-07-29 00:36:39 +0300

    refact: swap args in `DiscordGuild.getMember`

--- src/DiscordGuild.fs
+++ src/DiscordGuild.fs
@@ -1,7 +1,7 @@
 module DiscordBotExtensions.DiscordGuild
 open Types
 
-let getMember (guild: DSharpPlus.Entities.DiscordGuild) (user: DSharpPlus.Entities.DiscordUser) =
+let getMember (user: DSharpPlus.Entities.DiscordUser) (guild: DSharpPlus.Entities.DiscordGuild) =
     match user with
     | :? DSharpPlus.Entities.DiscordMember as guildMember -> guildMember
     | user -> await (guild.GetMemberAsync user.Id)
